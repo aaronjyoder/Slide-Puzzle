fa0ed7b9532df28112cf3c9c9ef916b5e27db09e
Aaron Yoder <aaronjyoder@gmail.com>
2021-03-14 13:17:34 -0400

    Add light/dark theme support

--- src/main/java/com/aaronjyoder/slide/Main.java
+++ src/main/java/com/aaronjyoder/slide/Main.java
@@ -1,23 +1,51 @@
 package com.aaronjyoder.slide;
 
 import com.aaronjyoder.slide.puzzle.Puzzle;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.jthemedetecor.OsThemeDetector;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 
+  public static final OsThemeDetector themeDetector = OsThemeDetector.getDetector();
+
   public static final String PUZZLE_PATH = "res/puzzle/default.png";
 
   public static void main(String[] args) {
+    if (themeDetector.isDark()) {
+      FlatDarkLaf.install();
+    } else {
+      FlatLightLaf.install();
+    }
+    themeDetector.registerListener(isDark -> {
+      if (isDark) {
+        try {
+          UIManager.setLookAndFeel(new FlatDarkLaf());
+          FlatLaf.updateUI();
+        } catch (UnsupportedLookAndFeelException e) {
+          e.printStackTrace();
+        }
+      } else {
+        try {
+          UIManager.setLookAndFeel(new FlatLightLaf());
+          FlatLaf.updateUI();
+        } catch (UnsupportedLookAndFeelException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+
     SwingUtilities.invokeLater(() -> {
       try {
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-      } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-        ex.printStackTrace();
+        Puzzle puzzle = new Puzzle(PUZZLE_PATH, 4);
+        puzzle.setVisible(true);
+      } catch (Exception e) {
+        System.exit(0);
       }
-      Puzzle puzzle = new Puzzle(PUZZLE_PATH, 4);
-      puzzle.setVisible(true);
     });
   }
 
